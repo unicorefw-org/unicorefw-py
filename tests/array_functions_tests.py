2b1ecaa21340d332d1ba092135833e8b5cc6f352
Kenny Ngo <kenny@forvn.com>
2025-03-31 03:05:28 -0400

    Setting up tests

--- tests/array_functions_tests.py
+++ tests/array_functions_tests.py
@@ -14,7 +14,7 @@ import sys
 import os
 
 # Add the src directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from unicorefw import UniCoreFW  # Now you can import Unicore as usual
 
 class TestUnicoreArrays(unittest.TestCase):
--- tests/object_functions_tests.py
+++ tests/object_functions_tests.py
@@ -16,7 +16,7 @@ import sys
 import os
 
 # Add the src directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from unicorefw import UniCoreFW  # Now you can import Unicore as usual
 
 class TestUniCoreFWObjects(unittest.TestCase):
