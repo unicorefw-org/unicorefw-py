99b5fe5005293ac0417ec2f759ae6abe108c629d
Kenny Ngo <kenny@forvn.com>
2025-12-27 17:47:54 -0500

    Fixed errors for github workflows

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="unicorefw",
-    version="1.1.0",
+    version="1.1.1",
     author="Kenny Ngo",
     author_email="kenny@unicorefw.org",
     description="Universal Core Utility Library",
